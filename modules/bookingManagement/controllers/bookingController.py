e497d4631658ad16935609359a82e78464b3ba53
samanHmd <saman.hamidi1993@gmail.com>
2023-06-19 23:26:50 -0400

    cancelation email added

--- modules/bookingManagement/controllers/bookingController.py
+++ modules/bookingManagement/controllers/bookingController.py
@@ -10,8 +10,10 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 import ssl
 from modules.bookingManagement.models.booking import Booking
-
-
+from modules.userManagement.models.user import User
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import ssl
 
 
 
@@ -35,5 +37,32 @@ class BookingController(Resource):
         booking = Booking.query.get(data['bookingId'])
         booking.isCanceled = True
         db.session.commit() 
+
+        user = User.query.get(booking['customerId'])
+
+        #Email
+        if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+            getattr(ssl, '_create_unverified_context', None)): 
+            ssl._create_default_https_context = ssl._create_unverified_context
+        message = Mail(
+            from_email='travelapplicationconcordia@gmail.com',
+            to_emails=user.email,
+        )
+        message.template_id = 'd-255889ec5c4d48b4bf0c3a1de4e13ed6'  # Replace with your template ID
+
+        message.dynamic_template_data = {
+        'name': user.name,
+        'ID': booking.id
+        }
+        try:
+            print(os.environ.get('SENDGRID_API_KEY'))
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e.message)
+
         bookings = Booking.query.all()
         return { "status": "Success", "bookings": [booking.as_dict() for booking in bookings] }
