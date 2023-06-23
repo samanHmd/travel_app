950e6ea6b78ecfc7a0d3b4ea959b2e3ba3ba8e0e
samanHmd <saman.hamidi1993@gmail.com>
2023-06-23 19:36:47 -0400

    returnComponent method add

--- modules/packageManagement/controllers/receiveDataController.py
+++ modules/packageManagement/controllers/receiveDataController.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, fields, marshal_with, marshal
 from modules.packageManagement.models.flight import Flight
 from modules.packageManagement.models.hotel import Hotel
 from modules.packageManagement.models.activity import Activity
+from modules.packageManagement.models.package import Package
 from modules import bcrypt, db, app
 import jwt
 from datetime import datetime, timedelta
@@ -14,26 +15,6 @@ class ReceiveDataController(Resource):
 
     def post(self):
         data = request.get_json()
-
-        required_fields = ['departureCity', 'destinationCity', 'check_in_date', 'check_out_date']
-        for field in required_fields:
-            if field not in data or not data[field]:
-                return {"error": f"{field} is required."}, 400
-
-        try:
-            check_in_date = datetime.strptime(data["check_in_date"], '%Y-%m-%d')
-            check_out_date = datetime.strptime(data["check_out_date"], '%Y-%m-%d')
-        except ValueError:
-            return {"error": "Dates must be in 'YYYY-MM-DD' format."}, 400
-
-        flights = Flight.query.filter_by(departureCity= data.get('departureCity'), arrivalCity=data.get('destinationCity')).all()
-        hotels = Hotel.query.filter_by(cityName=data.get('destinationCity')).all()
-        activities = Activity.query.all()
-        print(flights)
-        return {
-            "status": "success",
-            "flights": [flight.as_dict() for flight in flights],
-            "hotels": [hotel.as_dict() for hotel in hotels],
-            "activities": [activity.as_dict() for activity in activities]
-            }, 200
+        returnComponents_response = Package.returnComponents(data)
+        return data
             
--- modules/packageManagement/models/package.py
+++ modules/packageManagement/models/package.py
@@ -329,6 +329,32 @@ class Package(db.Model):
 
 
 
+    def returnComponents(data):
+        required_fields = ['departureCity', 'destinationCity', 'check_in_date', 'check_out_date']
+        for field in required_fields:
+            if field not in data or not data[field]:
+                return {"error": f"{field} is required."}, 400
+
+        try:
+            check_in_date = datetime.strptime(data["check_in_date"], '%Y-%m-%d')
+            check_out_date = datetime.strptime(data["check_out_date"], '%Y-%m-%d')
+        except ValueError:
+            return {"error": "Dates must be in 'YYYY-MM-DD' format."}, 400
+
+        flights = Flight.query.filter_by(departureCity= data.get('departureCity'), arrivalCity=data.get('destinationCity')).all()
+        hotels = Hotel.query.filter_by(cityName=data.get('destinationCity')).all()
+        activities = Activity.query.all()
+        print(flights)
+        return {
+            "status": "success",
+            "flights": [flight.as_dict() for flight in flights],
+            "hotels": [hotel.as_dict() for hotel in hotels],
+            "activities": [activity.as_dict() for activity in activities]
+            }, 200
+
+
+
+
 class PackageFlight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     packageId = db.Column(db.Integer, db.ForeignKey('package.id'))
